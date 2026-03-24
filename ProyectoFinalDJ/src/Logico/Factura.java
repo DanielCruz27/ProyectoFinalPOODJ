92ff2a944e8ec0df05863893d94150bc419baac0
Daniel Cruz <Daniel Cruz@LAPTOP-N1GRBTES>
2026-03-24 10:39:07 -0400

    Se crearon los atributos de factura, metodo de pago y de ticket

--- ProyectoFinalDJ/src/Logico/Factura.java
+++ ProyectoFinalDJ/src/Logico/Factura.java
@@ -1,5 +1,100 @@
 package Logico;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Factura {
+
+	private String idFactura;
+	private LocalDate fechaEmision;
+	private float montoTotal;
+	private boolean estadoPago;
+	private MetodoDePago metodoUtilizado;
+	private float itbis;
+	private Cliente elCliente;
+	private ArrayList<Servicio>losServicios;
+
+	public Factura(String idFactura, LocalDate fechaEmision, float montoTotal, boolean estadoPago,
+			MetodoDePago metodoUtilizado, float itbis, Cliente elCliente, ArrayList<Servicio> losServicios) {
+		super();
+		this.idFactura = idFactura;
+		this.fechaEmision = fechaEmision;
+		this.montoTotal = montoTotal;
+		this.estadoPago = estadoPago;
+		this.metodoUtilizado = metodoUtilizado;
+		this.itbis = itbis;
+		this.elCliente = elCliente;
+
+		if (losServicios == null) {
+			this.losServicios = new ArrayList<Servicio>();
+
+		} else {
+			this.losServicios = losServicios;
+		}
+	}
+
+	public String getIdFactura() {
+		return idFactura;
+	}
+
+	public void setIdFactura(String idFactura) {
+		this.idFactura = idFactura;
+	}
+
+	public LocalDate getFechaEmision() {
+		return fechaEmision;
+	}
+
+	public void setFechaEmision(LocalDate fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+
+	public float getMontoTotal() {
+		return montoTotal;
+	}
+
+	public void setMontoTotal(float montoTotal) {
+		this.montoTotal = montoTotal;
+	}
+
+	public boolean isEstadoPago() {
+		return estadoPago;
+	}
+
+	public void setEstadoPago(boolean estadoPago) {
+		this.estadoPago = estadoPago;
+	}
+
+	public MetodoDePago getMetodoUtilizado() {
+		return metodoUtilizado;
+	}
+
+	public void setMetodoUtilizado(MetodoDePago metodoUtilizado) {
+		this.metodoUtilizado = metodoUtilizado;
+	}
+
+	public float getItbis() {
+		return itbis;
+	}
+
+	public void setItbis(float itbis) {
+		this.itbis = itbis;
+	}
+
+	public Cliente getElCliente() {
+		return elCliente;
+	}
+
+	public void setElCliente(Cliente elCliente) {
+		this.elCliente = elCliente;
+	}
+
+	public ArrayList<Servicio> getLosServicios() {
+		return losServicios;
+	}
+
+	public void setLosServicios(ArrayList<Servicio> losServicios) {
+		this.losServicios = losServicios;
+	}
 
 }
--- ProyectoFinalDJ/src/Logico/Ticket.java
+++ ProyectoFinalDJ/src/Logico/Ticket.java
@@ -1,5 +1,62 @@
 package Logico;
 
+import java.time.LocalDate;
+
 public class Ticket {
+
+	private String idTicket;
+	private Cliente elCliente;
+	private String areaAtencion;
+	private LocalDate horaGeneracion;
+	private int prioridad;
+
+	public Ticket(String idTicket, Cliente elCliente, String areaAtencion, LocalDate horaGeneracion, int prioridad) {
+		super();
+		this.idTicket = idTicket;
+		this.elCliente = elCliente;
+		this.areaAtencion = areaAtencion;
+		this.horaGeneracion = horaGeneracion;
+		this.prioridad = prioridad;
+	}
+
+	public String getIdTicket() {
+		return idTicket;
+	}
+
+	public void setIdTicket(String idTicket) {
+		this.idTicket = idTicket;
+	}
+
+	public Cliente getElCliente() {
+		return elCliente;
+	}
+
+	public void setElCliente(Cliente elCliente) {
+		this.elCliente = elCliente;
+	}
+
+	public String getAreaAtencion() {
+		return areaAtencion;
+	}
+
+	public void setAreaAtencion(String areaAtencion) {
+		this.areaAtencion = areaAtencion;
+	}
+
+	public LocalDate getHoraGeneracion() {
+		return horaGeneracion;
+	}
+
+	public void setHoraGeneracion(LocalDate horaGeneracion) {
+		this.horaGeneracion = horaGeneracion;
+	}
+
+	public int getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
 
 }
