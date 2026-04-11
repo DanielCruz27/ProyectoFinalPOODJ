a8e1b73a8002c4ed89dff9d0f1ecacbf2c67d237
jemv0001 <jemv0001@ce.pucmm.edu.do>
2026-04-11 17:45:28 -0400

    pull de seguridad

--- ProyectoFinalDJ/src/Visual/Principal_InicioSesion.java
+++ ProyectoFinalDJ/src/Visual/Principal_InicioSesion.java
@@ -161,9 +161,7 @@ public class Principal_InicioSesion extends JFrame {
                 } else {
                     JOptionPane.showMessageDialog(null, "Credenciales incorrectas", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                SistemaPrincipal sistema = new SistemaPrincipal("administrador");
-                sistema.setVisible(true);
-                dispose();
+                
             }
         });
         btnAcceder.setBackground(new Color(0, 128, 255));
